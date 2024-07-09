08cc1ab8f5b5b759ac05b7b48ef7a1b9c6a0af52
dima <pestuno@gmail.com>
2024-07-10 00:07:52 +0700

    sqlalchemy first model

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template, url_for
-import os
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from config import DevConfig as myConfig
+
 import dachadb, dachacogitare
 
 
-# config const
-DATABASE = dachadb.DBUNITS
-DEBUG = True
-SECRET_KEY = 'kgjdk?R$ghdk>jkljvv4etr3240rfd'
-
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.update(dict(DATABASE=os.path.join(app.root_path,'units.db')))
+app.config.from_object(myConfig)
+db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column("user_name", db.String(255))
+    password = db.Column("password", db.String(255))
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return f"<User '{self.username}'>"
 
 @app.route('/')
 @app.route('/index/')
 def index():
     print(url_for('index'))
     return render_template('index.html',
-                           menu = 'Home')
+                           menu='Home')
 
 @app.route('/control/')
 def controlpanel():
@@ -27,13 +34,13 @@ def controlpanel():
     cols = ['unit_name','unit_category','unit_desc']
     units = dachadb.selectFromDB('units.db3','units', cols)
     print(units)
-    tree = {'security':['perimeter', 'lockers', 'fenestra', 'porta'],
-            'luminos':['home', 'garden', 'entrance'],
-            'ferma':['feed Cat', 'feed Bunny', 'feed Fish']}
+    tree = {'security': ['perimeter', 'lockers', 'fenestra', 'porta'],
+            'luminos': ['home', 'garden', 'entrance'],
+            'ferma': ['feed Cat', 'feed Bunny', 'feed Fish']}
     return render_template('controlpanel.html',
-                           units = units,
-                           tree = tree,
-                           menu = 'Control panel')
+                           units=units,
+                           tree=tree,
+                           menu='Control panel')
 
 @app.route('/dop/')
 def index1():
@@ -41,6 +48,6 @@ def index1():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5003, host='0.0.0.0')
+    app.run()
 
 
